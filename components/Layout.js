b3e01f1c8913eef7414677992cd390bbe07bd5f1
sahil <sahilsiddiqui.dev@gmail.com>
2021-11-02 11:28:28 +0530

    master: add material style

--- components/Layout.js
+++ components/Layout.js
@@ -1,23 +1,26 @@
 import { AppBar, Container, Toolbar, Typography } from "@material-ui/core";
 import Head from "next/head";
 import React from "react";
+import useStyles from "./utils/styles";
 
 export default function Layout({ children }) {
+  const classes = useStyles();
+
   return (
     <div>
       <Head>
         <title>shopup</title>
       </Head>
 
-      <AppBar position="static">
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>shopup</Typography>
         </Toolbar>
       </AppBar>
 
-      <Container>{children}</Container>
+      <Container className={classes.main}>{children}</Container>
 
-      <footer>
+      <footer className={classes.footer}>
         <Typography>All rights reserved. Shopup.</Typography>
       </footer>
     </div>
