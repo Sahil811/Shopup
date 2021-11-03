aa8c2b23697d4471c4fbe08f415644b0474af9fe
sahil <sahil@qburst.com>
2021-11-03 15:19:31 +0530

    main: material ui theme in project details page

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,9 @@ import {
   Link,
   Toolbar,
   Typography,
+  createTheme,
+  ThemeProvider,
+  CssBaseline,
 } from "@material-ui/core";
 import Head from "next/head";
 import React from "react";
@@ -11,6 +14,30 @@ import useStyles from "../utils/styles.js";
 import NextLink from "next/link";
 
 export default function Layout({ title, description, children }) {
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: "1.6rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+      h2: {
+        fontSize: "1.4rem",
+        fontWeight: 400,
+        margin: "1rem 0",
+      },
+    },
+    palette: {
+      type: "light",
+      primary: {
+        main: "#f0c000",
+      },
+      secondary: {
+        main: "#208080",
+      },
+    },
+  });
+
   const classes = useStyles();
 
   return (
@@ -20,33 +47,36 @@ export default function Layout({ title, description, children }) {
         {description && <meta name="description" content={description} />}
       </Head>
 
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="" passHref>
-            <Link className={classes.link}>
-              <Typography className={classes.brand}>shopup</Typography>
-            </Link>
-          </NextLink>
-
-          <div className={classes.grow}></div>
-
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link className={classes.link}>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="" passHref>
+              <Link className={classes.link}>
+                <Typography className={classes.brand}>shopup</Typography>
+              </Link>
             </NextLink>
 
-            <NextLink href="/login" passHref>
-              <Link className={classes.link}>login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
+            <div className={classes.grow}></div>
 
-      <Container className={classes.main}>{children}</Container>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link className={classes.link}>Cart</Link>
+              </NextLink>
 
-      <footer className={classes.footer}>
-        <Typography>All rights reserved. Shopup.</Typography>
-      </footer>
+              <NextLink href="/login" passHref>
+                <Link className={classes.link}>login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+
+        <Container className={classes.main}>{children}</Container>
+
+        <footer className={classes.footer}>
+          <Typography>All rights reserved. Shopup.</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
