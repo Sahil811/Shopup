0bd7fcfcbf9c1c6649b478446b1bda4a3813bb9c
sahil <sahil@qburst.com>
2021-11-11 17:20:30 +0530

    main: Deliver order by Admin

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -18,12 +18,15 @@ import {
   Card,
   List,
   ListItem,
+  Button,
 } from "@material-ui/core";
 import axios from "axios";
+import { useSnackbar } from "notistack";
 import { useRouter } from "next/router";
 import useStyles from "../../utils/styles";
 import { getError } from "../../utils/error";
 import { PayPalButtons, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import moment from "moment";
 
 function reducer(state, action) {
   switch (action.type) {
@@ -41,6 +44,20 @@ function reducer(state, action) {
       return { ...state, loadingPay: false, errorPay: action.payload };
     case "PAY_RESET":
       return { ...state, loadingPay: false, successPay: false, errorPay: "" };
+
+    case "DELIVER_REQUEST":
+      return { ...state, loadingDeliver: true };
+    case "DELIVER_SUCCESS":
+      return { ...state, loadingDeliver: false, successDeliver: true };
+    case "DELIVER_FAIL":
+      return { ...state, loadingDeliver: false, errorDeliver: action.payload };
+    case "DELIVER_RESET":
+      return {
+        ...state,
+        loadingDeliver: false,
+        successDeliver: false,
+        errorDeliver: "",
+      };
     default:
       state;
   }
@@ -50,20 +67,21 @@ function Order({ params }) {
   const orderId = params.id; // Need to be same with [id].js
 
   const [{ isPending }, paypalDispatch] = usePayPalScriptReducer();
+  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
   const classes = useStyles();
   const router = useRouter();
   const { state } = useContext(Store);
   const { userInfo } = state;
 
-  const [{ loading, error, order, successPay }, dispatch] = useReducer(
-    reducer,
-    {
-      loading: true,
-      order: {},
-      error: "",
-    }
-  );
+  const [
+    { loading, error, order, successPay, loadingDeliver, successDeliver },
+    dispatch,
+  ] = useReducer(reducer, {
+    loading: true,
+    order: {},
+    error: "",
+  });
 
   const {
     shippingAddress,
@@ -97,11 +115,18 @@ function Order({ params }) {
       }
     };
 
-    if (!order._id || successPay || (order._id && order._id !== orderId)) {
+    if (
+      !order._id ||
+      successPay ||
+      successDeliver ||
+      (order._id && order._id !== orderId)
+    ) {
       fetchOrder();
-
       if (successPay) {
         dispatch({ type: "PAY_RESET" });
+      }
+      if (successDeliver) {
+        dispatch({ type: "DELIVER_RESET" });
       }
     } else {
       const loadPaypalScript = async () => {
@@ -125,7 +150,7 @@ function Order({ params }) {
       };
       loadPaypalScript();
     }
-  }, [order, successPay]);
+  }, [order, successPay, successDeliver]);
 
   // Call action from Paypal to create the order
   const createOrder = (data, actions) => {
@@ -173,9 +198,27 @@ function Order({ params }) {
 
   // This function will be called when there is an error in the Paypal payment
 
-  function onError(err) {
+  const onError = (err) => {
     enqueueSnackbar(getError(err), { variant: "error" });
-  }
+  };
+
+  const deliverOrderHandler = async () => {
+    try {
+      dispatch({ type: "DELIVER_REQUEST" });
+      const { data } = await axios.put(
+        `/api/orders/${order._id}/deliver`,
+        {},
+        {
+          headers: { authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({ type: "DELIVER_SUCCESS", payload: data });
+      enqueueSnackbar("Order is delivered", { variant: "success" });
+    } catch (err) {
+      dispatch({ type: "DELIVER_FAIL", payload: getError(err) });
+      enqueueSnackbar(getError(err), { variant: "error" });
+    }
+  };
 
   return (
     <Layout title={`Order ${orderId}`}>
@@ -219,7 +262,10 @@ function Order({ params }) {
                 </ListItem>
                 <ListItem>{paymentMethod}</ListItem>
                 <ListItem>
-                  Status: {isPaid ? `paid at ${paidAt}` : "not paid"}
+                  Status:{" "}
+                  {isPaid
+                    ? `paid at ${moment(paidAt).format("MMM DD, YYYY")}`
+                    : "not paid"}
                 </ListItem>
               </List>
             </Card>
@@ -353,6 +399,22 @@ function Order({ params }) {
                       )}
                     </ListItem>
                   )}
+
+                  {userInfo?.user?.isAdmin &&
+                    order.isPaid &&
+                    !order.isDelivered && (
+                      <ListItem>
+                        {loadingDeliver && <CircularProgress />}
+                        <Button
+                          fullWidth
+                          variant="contained"
+                          color="primary"
+                          onClick={deliverOrderHandler}
+                        >
+                          Deliver Order
+                        </Button>
+                      </ListItem>
+                    )}
                 </ListItem>
               </List>
             </Card>
