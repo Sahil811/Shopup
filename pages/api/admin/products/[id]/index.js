756dab3818306413672f210cb4fbe3e2212daa4f
sahil <sahil@qburst.com>
2021-11-12 15:51:22 +0530

    main: Ceate-Delete-Product from Admin dashboard

--- pages/api/admin/products/[id]/index.js
+++ pages/api/admin/products/[id]/index.js
@@ -6,12 +6,16 @@ import { isAdmin, isAuth } from "../../../../../utils/auth";
 const handler = nc();
 handler.use(isAuth, isAdmin);
 
+/// GET PRODUCT ///
+
 handler.get(async (req, res) => {
   await db.connect();
   const product = await Product.findById(req.query.id);
   await db.disconnect();
   res.send(product);
 });
+
+/// EDIT PRODUCT ///
 
 handler.put(async (req, res) => {
   await db.connect();
@@ -28,6 +32,21 @@ handler.put(async (req, res) => {
     await product.save();
     await db.disconnect();
     res.send({ message: "Product Updated Successfully" });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: "Product Not Found" });
+  }
+});
+
+/// DELETE PRODUCT ///
+
+handler.delete(async (req, res) => {
+  await db.connect();
+  const product = await Product.findById(req.query.id);
+  if (product) {
+    await product.remove();
+    await db.disconnect();
+    res.send({ message: "Product Deleted" });
   } else {
     await db.disconnect();
     res.status(404).send({ message: "Product Not Found" });
